95348ef644c5f4d067df55a95774823b0f36ff1e
jalehoux <jalehoux@gmail.com>
2018-07-10 12:22:17 -0400

    testing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config;
 const request = require('request');
 const app = express()
 
-app.get('/', (req, res) => res.send('test'))
+app.get('/', (req, res) => res.send(body))
 
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
 
@@ -11,4 +11,5 @@ request('https://swapi.co/api/people/1', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
+  var test = body;
 });
