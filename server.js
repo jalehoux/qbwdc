cfec17f5a1af1229cf16a1f3581c036fd28dca22
jalehoux <jalehoux@gmail.com>
2018-07-10 13:11:12 -0400

    added structure

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express')
-const dotenv = require('dotenv').config;
+const dotenv = require('dotenv').config();
 const request = require('request');
 const app = express()
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(body))
+app.get('/', (req, res) => res.send('Hello World'))
 
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
 
-request('https://swapi.co/api/people/1', function (error, response, body) {
+request('https://primebuchholz.quickbase.com/db/'+process.env.QB_TABLE_ID+'?a=API_GetSchema&usertoken='+process.env.QB_USER_TOKEN, function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
-  var test = body;
 });
