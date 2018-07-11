c8fee52372e6e535b07b9a5abc252b095d45a26f
jalehoux <jalehoux@gmail.com>
2018-07-11 13:54:53 -0400

    updated with static pages

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+console.log("Tableau We Data Connector Fired Up");
+(function () {
+    var myConnector = tableau.makeConnector();
+
+    myConnector.getSchema = function (schemaCallback) {
+
+    };
+
+    myConnector.getData = function (table, doneCallback) {
+
+    };
+
+    tableau.registerConnector(myConnector);
+})();
+
+$(document).ready(function () {
+    $("#submitButton").click(function () {
+        tableau.connectionName = "USGS Earthquake Feed";
+        tableau.submit();
+    });
+});
--- server.js
+++ server.js
@@ -1,35 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config();
-const request = require('request');
-const xml2js = require('xml2js').parseString;
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const options = {  
-  url: 'https://primebuchholz.quickbase.com/db/'+process.env.QB_TABLE_ID+'?a=API_GetSchema&usertoken='+process.env.QB_USER_TOKEN,
-  method: 'GET',
-  headers: {
-  }
-};
+//used for pug see view folder
+// app.set('view engine', 'pug');
+// app.get('/', (req, res) => res.render('index',{title: 'Quick Base WDC'}));
 
-app.set('view engine', 'pug');
-
-app.get('/', (req, res) => res.render('index',{title: 'Quick Base WDC', message: 'Hello World!'}));
+//used for static web pages
+app.use('/', express.static(__dirname + '/public'));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
-
-
-//json return
-request(options, function(err, res, body) {
-  let json = xml2js(body, function(err, result){
-    console.log(result);
-  });
-});
-
-//xml return
-request(options, function (error, response, body) {
-  var test = body;
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
