44dfcc337d5dbadc9ff6f73314f01a75781bed9f
jalehoux <jalehoux@gmail.com>
2018-07-19 12:16:02 -0400

    closer and closer

--- server.js
+++ server.js
@@ -1,43 +1,32 @@
 const express = require('express')
 const dotenv = require('dotenv').config();
 const bodyParser = require("body-parser");
-const request = require('request');
+const QuickBase = require('quickbase'); 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-var options = {  
-    url: 'https://primebuchholz.quickbase.com/db/'+process.env.QB_TABLE_ID+'?a=API_GetSchema&usertoken='+process.env.QB_USER_TOKEN,
-    method: 'GET',
-    headers: {
-    }
-  };
 
-// //used for pug see view folder
-// app.set('view engine', 'pug');
-// app.get('/', (req, res) => res.render('index',{title: 'Quick Base WDC'}));
+const quickbase = new QuickBase({
+  realm: 'primebuchholz',
+  userToken: process.env.QB_USER_TOKEN,
+});
 
-//used for static web pages
-app.use('/', express.static(__dirname + '/public'));
+//used for pug see view folder
+app.set('view engine', 'pug');
+app.get('/', (req, res) => res.render('index',{title: 'Quick Base WDC'}));
 
 //handeling post information
-app.post('/index', urlencodedParser, function(req, res) {
-    console.log(req.body);
-    var returns;
-    request(options, function (error, response, body) {
-        returns = body
-        console.log(returns);
-      });
+app.get('/index', urlencodedParser, function(req, res) {
+  var appid = req.query.appid;
+  quickbase.api('API_GetSchema', {
+    dbid: appid
+  }).then((results)=>{
+    res.send(results);
+  });
 })
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
-
-
-
-
-
-
-
 
 
 //used for static web pages
